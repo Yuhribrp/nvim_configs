ce8af5ea85bf7e9a7a31050594e6c504d6961369
YuhriParada <yuhriparada@gmail.com>
2024-06-25 22:48:24 -0300

    neovim essentials config to use

---
+++ lua/vim-options.lua
@@ -0,0 +1,19 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+vim.opt.termguicolors = true
+vim.opt.number = true
+vim.opt.ruler = true
+vim.opt.laststatus = 2
+vim.opt.clipboard = "unnamedplus"
+
+if vim.fn.has("wsl") == 1 then
+	vim.api.nvim_create_autocmd("TextYankPost", {
+		group = vim.api.nvim_create_augroup("Yank", { clear = true }),
+		callback = function()
+			vim.fn.system("clip.exe", vim.fn.getreg('"'))
+		end,
+	})
+end
