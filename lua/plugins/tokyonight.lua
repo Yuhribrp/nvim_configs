31a21e4b1c56d0ace2be08d3b127497b8eabb993
YuhriParada <yuhriparada@gmail.com>
2024-07-02 14:29:57 -0300

    usgin cattpuccin as theme

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,9 +1,36 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd[[colorscheme tokyonight-night]]
-  end
+	-- {
+	-- 	"folke/tokyonight.nvim",
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	opts = {},
+	-- 	config = function()
+	-- 		-- vim.cmd([[colorscheme tokyonight-night]])
+	-- 	end,
+	-- },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			local catp = require("catppuccin")
+
+			catp.setup({
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					-- light = "latte",
+					dark = "mocha",
+				},
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = true,
+					barbar = true,
+				},
+			})
+			vim.cmd([[colorscheme catppuccin]])
+		end,
+	},
 }
