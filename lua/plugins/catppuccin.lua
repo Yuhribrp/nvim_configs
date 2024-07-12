07486462fa1961835c0c283b96bb4e74a16037fa
YuhriParada <yuhriparada@gmail.com>
2024-07-11 21:32:13 -0300

    renamed theme file to cattpuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,13 +1,4 @@
 return {
-	-- {
-	-- 	"folke/tokyonight.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	opts = {},
-	-- 	config = function()
-	-- 		-- vim.cmd([[colorscheme tokyonight-night]])
-	-- 	end,
-	-- },
 	{
 		"catppuccin/nvim",
 		lazy = false,
