8481e63182a482cbeb4d8260d6572b926fc3aac5
YuhriParada <yuhriparada@gmail.com>
2024-10-14 10:40:55 -0300

    cyberdream theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,27 +1,49 @@
-return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			local catp = require("catppuccin")
+-- return {
+-- 	{
+-- 		"catppuccin/nvim",
+-- 		lazy = false,
+-- 		priority = 1000,
+-- 		config = function()
+-- 			local catp = require("catppuccin")
 
-			catp.setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				background = { -- :h background
-					-- light = "latte",
-					dark = "mocha",
-				},
-				integrations = {
-					cmp = true,
-					gitsigns = true,
-					nvimtree = true,
-					treesitter = true,
-					notify = true,
-					barbar = true,
-				},
-			})
-			vim.cmd([[colorscheme catppuccin]])
-		end,
-	},
+-- 			catp.setup({
+-- 				flavour = "mocha", -- latte, frappe, macchiato, mocha
+-- 				background = { -- :h background
+-- 					-- light = "latte",
+-- 					dark = "mocha",
+-- 				},
+-- 				integrations = {
+-- 					cmp = true,
+-- 					gitsigns = true,
+-- 					nvimtree = true,
+-- 					treesitter = true,
+-- 					notify = true,
+-- 					barbar = true,
+-- 				},
+-- 			})
+-- 			vim.cmd([[colorscheme catppuccin]])
+-- 		end,
+-- 	},
+-- }
+--
+
+return {
+{
+  "scottmckendry/cyberdream.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    local cyberd = require("cyberdream")
+
+    cyberd.setup({
+      transparent = false,
+      italic_comments = true,
+      hide_fillchars = false,
+      borderless_telescope = false,
+      terminal_colors = true,
+      cache = false,
+      })
+      vim.cmd([[colorscheme cyberdream]])
+    end,
+  },
 }
