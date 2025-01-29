8dec38e184e427bd1fded5a4be7786f02cb1f224
Yuhri Benaion Ramos Parada <yuhriparada@gmail.com>
2025-01-29 02:28:33 -0300

    ruby-lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -13,6 +13,7 @@ return {
 			require("mason-lspconfig").setup({
 				ensure_installed = {
 					"lua_ls",
+          "ruby_lsp",
 					-- "omnisharp",
           -- "csharp_ls",
 					"solargraph",
@@ -29,6 +30,9 @@ return {
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
+      lspconfig.ruby_lsp.setup({
+        capabilities = capabilities,
+      })
 			-- lspconfig.omnisharp.setup({
 			-- 	capabilities = capabilities,
 			-- })
