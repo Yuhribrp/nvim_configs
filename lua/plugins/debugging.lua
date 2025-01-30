e649748b65620d7fa7ea9ed77facb7d842d3d954
Yuhribrp <yuhriparada@gmail.com>
2025-01-30 16:47:53 -0300

    rustacean with debugger and crates with version autocomplete

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,32 +1,53 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"suketa/nvim-dap-ruby",
-		"rcarriga/nvim-dap-ui",
-		"folke/trouble.nvim",
-	},
-	config = function()
-		local dap = require("dap")
-		local dapui = require("dapui")
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "suketa/nvim-dap-ruby",
+    "rcarriga/nvim-dap-ui",
+    "folke/trouble.nvim",
+  },
+  config = function()
+    local dap = require("dap")
+    local dapui = require("dapui")
 
-		require("dapui").setup()
+    require("dapui").setup()
     require("dap-ruby").setup()
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
 
-		vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-	end,
+
+    -- Old keymaps commented for now
+    -- vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+    -- vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+
+    -- Nvim DAP
+    vim.keymap.set("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+    vim.keymap.set("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+    vim.keymap.set("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+    vim.keymap.set("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+    vim.keymap.set("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>",
+      { desc = "Debugger toggle breakpoint" })
+    vim.keymap.set(
+      "n",
+      "<Leader>dd",
+      "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+      { desc = "Debugger set conditional breakpoint" }
+    )
+    vim.keymap.set("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+    vim.keymap.set("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+
+    -- rustaceanvim
+    vim.keymap.set("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
+  end,
 }
